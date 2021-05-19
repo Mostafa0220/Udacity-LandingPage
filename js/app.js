5008ef1a75db6722f96d49d16f1959c1f1d4f074
Mostafa <mostafa0220@gmail.com>
2021-05-19 06:12:34 +0400

    add back to top button

--- js/app.js
+++ js/app.js
@@ -19,6 +19,8 @@
  */
 let navUnorderedList = document.getElementById("nav-unordered-list");
 let sectionsCount = document.getElementsByClassName("landing__container").length;
+//Get the back to top button
+const backToTopBtn = document.getElementById("back-to-top-btn");
 /**
  * End Global Variables
  * 
@@ -56,10 +58,35 @@ for (i = 1; i < sectionsCount + 1; i++) {
 
     });
 }
+/**
+ * Back to top button using this example  
+ * https: //www.w3schools.com/howto/howto_js_scroll_to_top.asp
+ */
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 
 // Add class 'active' to section when near top of viewport
 
+
+
+// Set sections as active
 
 
 // Set sections as active
